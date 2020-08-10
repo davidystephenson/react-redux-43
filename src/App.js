e316bf35c6f9be59d902ad4e24d4763b1fac6f5d
David Y. Stephenson <david@davidystephenson.com>
2020-08-10 14:53:37 +0200

    Order burgers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
 
 function App() {
+  const [index, setIndex] = useState(0)
+
+  // Get something out of the store to show it
+  function selector (store) {
+    return store[index]
+  }
+
+  const hamburger = useSelector(selector)
+
+  const dispatch = useDispatch()
+
+  console.log('hamburger test:', hamburger)
+
+  function onChange (event) {
+    setIndex(event.target.value)
+  }
+
+  function onClick () {
+    // 1. Make the action object
+    // type - string that matches a reducer case
+    // payload? - anything else that describes the behavior
+    const action = {
+      type: 'NEW_ORDER', payload: hamburger.meat
+    }
+
+    // 2. Dispatch the action to the store
+    dispatch(action)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <input
+        type='range'
+        max='2'
+        min='0'
+        onChange={onChange}
+        value={index}
+      />
+
+      <h1>{hamburger.meat}</h1>
+
+      <p>
+        Does it have lettuce?
+        {' '}
+        {hamburger.lettuce ? 'Yes' : 'No'}
+      </p>
+
+      <h4>{hamburger.orders} happy customers :)</h4>
+
+      <button onClick={onClick}>Order now!</button>
+    </>
+  )
 }
 
 export default App;
---
+++ src/reducer.js
@@ -0,0 +1,64 @@
+const initial = [
+  {
+    meat: 'angus beef',
+    lettuce: false,
+    tomato: true,
+    sauces: ['ketchup', 'mayonnaise'],
+    orders: 0
+  },
+  {
+    meat: 'wagyu beef',
+    lettuce: true,
+    tomato: true,
+    sauces: ['truffle oil'],
+    orders: 0
+  },
+  {
+    meat: 'chicken',
+    lettuce: false,
+    tomato: false,
+    sauces: ['ketchup'],
+    orders: 0
+  }
+]
+
+// { type: 'NEW_ORDER', payload: 'chicken' }
+
+function reducer (state = initial, action) {
+  console.log('action test:', action)
+  switch (action.type) {
+    case 'NEW_ORDER': {
+      // WRONG (mutating, assignment, = (equal signs))
+      // const burger = state
+      //   .find(burger => burger.meat === action.payload)
+      // burger.orders = burger.orders + 1
+
+      // RIGHT (returning, copying, ... (spread))
+      // 1. Make a totally new value
+      // of the whole state
+      console.log('oldState test:', state)
+      const newState = state.map(burger => {
+        console.log('oldBurger test:', burger)
+        if (burger.meat === action.payload) {
+          const newOrders = burger.orders + 1
+          const newBurger = { ...burger, orders: newOrders }
+          console.log('newBurger test:', newBurger)
+
+          return newBurger
+        }
+
+        return burger
+      })
+      console.log('newState test:', newState)
+
+      // 2. Return that new value
+      return newState
+    }
+
+    default: {
+      return state
+    }
+  }
+}
+
+export default reducer
